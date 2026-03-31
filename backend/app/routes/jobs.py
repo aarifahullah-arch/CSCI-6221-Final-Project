8668a9987afdc7241d9cd5c52a1cc7bb08011cb3
INNOCENTGWARA <101759018+INNOCENTGWARA@users.noreply.github.com>
2026-03-30 22:33:57 -0400

    Update jobs.py

--- backend/app/routes/jobs.py
+++ backend/app/routes/jobs.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.services.job_sources import fetch_remotive_jobs
 
 router = APIRouter()
-
+ 
 
 # Returns job data collected from external APIs
 @router.get("/")
